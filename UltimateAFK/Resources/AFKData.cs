8bff185cdf8f9004e08cfec50d5a8f463ea3d0ec
SrLicht <francoquiroz_13@live.com>
2022-05-14 14:38:38 -0300

    Update AFKData.cs

--- UltimateAFK/Resources/AFKData.cs
+++ UltimateAFK/Resources/AFKData.cs
@@ -1,27 +1,23 @@
-﻿using System.Collections.Generic;
-using UltimateAFK.Handlers.Components;
+﻿using Exiled.API.Features.Roles;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace UltimateAFK.Resources
 {
     public struct AFKData
     {
-        public AFKComponent AfkComp { get; set; }
+        public Vector3 Position { get; set; }
 
-        public Vector3 SpawnLocation { get; set; }
+        public RoleType Role { get; set; }
 
         public Dictionary<ItemType, ushort> Ammo { get; set; }
 
-        public List<ItemType> Items { get; set; }
+        public List<string> CustomItems { get; set; }
+
+        public List<Exiled.API.Features.Items.Item> Items { get; set; }
 
         public float Health { get; set; }
 
-        public bool Is079 { get; set; }
-
-        public byte Level { get; set; }
-
-        public float Xp { get; set; }
-
-        public float Energy { get; set; }
+        public Scp079Role SCP079Role { get; set; }
     }
 }
