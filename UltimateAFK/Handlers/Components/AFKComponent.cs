133218d0ee4a0351ba1b533d3d239c5f214fd11f
SrLicht <francoquiroz_13@live.com>
2022-05-14 14:37:41 -0300

    Update Component

--- UltimateAFK/Handlers/Components/AFKComponent.cs
+++ UltimateAFK/Handlers/Components/AFKComponent.cs
@@ -1,39 +1,37 @@
 ﻿using Exiled.API.Features;
 using Exiled.API.Features.Roles;
+using Exiled.CustomItems.API.Features;
 using Exiled.Events.EventArgs;
+using Exiled.Permissions.Extensions;
 using MEC;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using UltimateAFK.Resources;
 using UnityEngine;
 
 namespace UltimateAFK.Handlers.Components
 {
-    /// <summary>
-    /// Cringe componente for Players. Warning doing an AFKChecker is not a pretty thing to look at but I will try to document so you understand what I am trying to do :)
-    /// </summary>
     public class AFKComponent : MonoBehaviour
     {
-        #region Variables and Stuff
-        public Player MyPlayer { get; set; }
+        #region Variables
+        public Player MyPlayer;
+        public Player ReplacementPlayer = null;
 
         /// <summary>
         /// If True, the component will ignore this player, but will not be destroyed.
         /// </summary>
-        public bool IsDisable { get; set; }
+        public bool IsDisable = false;
+        // ------------->>>>> Sex <<<<------------
+        public Vector3 LastPosition;
+        public Vector3 LastRotation;
+        // -------------- Gwa gwa ---------------
+        public int AFKTime = 0;
+        public int AFKCount = 0;
 
-        public Player ReplacementPlayer { get; internal set; }
-
+        // Coroutine handle
         // Using a MEC Coroutine is more optimized than using Unity methods.
         private CoroutineHandle CountHandler;
-
-        public Vector3 AFKLastPosition;
-
-        public Vector3 AFKLastAngle;
-
-        public int AFKTime;
-
-        public int AFKCount;
         #endregion
 
         private void Awake()
@@ -53,112 +51,84 @@ namespace UltimateAFK.Handlers.Components
 
             // Coroutine dies when the component or the ReferenceHub (Player) is destroyed.
             CountHandler = Timing.RunCoroutine(CheckAfkPerSecond().CancelWith(this).CancelWith(gameObject));
+
+            if (MyPlayer.CheckPermission("uafk.ignore"))
+            {
+                Log.Debug($"The player {MyPlayer.Nickname} has the permission \"uafk.ignore\" disabling component");
+                IsDisable = true;
+            }
         }
 
-        private void OnDestroy()
-        {
-            Destroy(true);
-        }
-
-        // With this you can destroy the component in a more controlled way.
-        public void Destroy(bool value = false)
+        public void Destroy()
         {
             try
             {
-                if (value)
-                {
-                    // This is to avoid a loop where the component constantly calls OnDestroy because Destroy calls OnDestroy and OnDestroy calls Destroy.
-                    Exiled.Events.Handlers.Player.Destroying -= OnDestroying;
-                    Exiled.Events.Handlers.Player.Jumping -= OnJumping;
-                }
-                else
-                {
-                    Exiled.Events.Handlers.Player.Destroying -= OnDestroying;
-                    Exiled.Events.Handlers.Player.Jumping -= OnJumping;
-                    Destroy(this);
-                }
+                MyPlayer = null;
+                ReplacementPlayer = null;
+
+                Exiled.Events.Handlers.Player.Destroying -= OnDestroying;
+                Exiled.Events.Handlers.Player.Jumping -= OnJumping;
+                Destroy(this);
             }
-            catch (Exception e)
+            catch (Exception)
             {
-                Log.Error($"Exception: {e}\n Couldn't destroy: {this}\nIs ReferenceHub null? {MyPlayer is null}");
+
+                throw;
             }
         }
 
-
-        private IEnumerator<float> CheckAfkPerSecond()
+        public void CheckAFK()
         {
-            while (true)
+            var cantcontinue = MyPlayer.IsDead || Player.List.Count() <= UltimateAFK.Instance.Config.MinPlayers || (UltimateAFK.Instance.Config.IgnoreTut && MyPlayer.IsTutorial) || Round.IsLobby;
+
+            if (!cantcontinue)
             {
-                if (!IsDisable)
-                {
-                    AFKChecker();
-                }
-
-                yield return Timing.WaitForSeconds(1.2f);
-            }
-        }
-
-        #region AFKChecker | CRINGE AAAAAAAAAAAAAAA 
-        private void AFKChecker()
-        {
-
-            bool cantContinue = MyPlayer.IsDead || Player.List.Count() <= UltimateAFK.Instance.Config.MinPlayers || (UltimateAFK.Instance.Config.IgnoreTut && MyPlayer.IsTutorial) || Round.IsLobby;
-
-            //Log.Debug($"Can continue ? || Player is Dead {MyPlayer.IsDead} || Player list is low {Player.List.Count() <= UltimateAFK.Instance.Config.MinPlayers} || Player is tutorial and config says no {UltimateAFK.Instance.Config.IgnoreTut && MyPlayer.IsTutorial} || Round is in lobby {Round.IsLobby}");
-            if (!cantContinue)
-            {
-
-                #region Check if player is 079 or 096
-                Scp079Role scp079Role = MyPlayer.Role as Scp079Role;
-                Scp096Role scp096Role = MyPlayer.Role as Scp096Role;
-
+                bool isSCP079 = MyPlayer.Role is Scp079Role;
+                bool isSCP096 = MyPlayer.Role is Scp096Role;
                 bool isNotCrying = false;
-                bool is079 = scp079Role != null;
-                bool is096 = scp096Role != null;
+                Scp079Role scp079role = null;
 
-                if (is096 || is079)
+                if (isSCP096 || isSCP079)
                 {
-                    if (is096)
+                    if (isSCP096)
                     {
-                        scp096Role = MyPlayer.Role as Scp096Role;
-                        isNotCrying = scp096Role.State == PlayableScps.Scp096PlayerState.TryNotToCry;
+                        var role = MyPlayer.Role as Scp096Role;
+                        isNotCrying = role.TryingNotToCry;
                     }
-                    else if (is079)
+                    else if (isSCP079)
                     {
-                        // Unnecessary, but whatever
-                        scp079Role = MyPlayer.Role as Scp079Role;
+                        scp079role = MyPlayer.Role as Scp079Role;
                     }
                 }
-                #endregion
 
                 Vector3 position = MyPlayer.Position;
-                Vector3 vector = is079 ? scp079Role.Camera.HeadPosition : (Vector3)MyPlayer.Rotation;
+                Vector3 vector = isSCP079 ? scp079role.Camera.HeadPosition : (Vector3)MyPlayer.Rotation;
 
-                bool isMoving = position != AFKLastPosition || vector != AFKLastAngle || isNotCrying;
+                bool isMoving = position != LastPosition || vector != LastRotation || isNotCrying;
 
                 if (isMoving)
                 {
-
-                    this.AFKLastPosition = position;
-                    this.AFKLastAngle = vector;
+                    this.LastPosition = position;
+                    this.LastRotation = vector;
                     this.AFKTime = 0;
-                    this.ReplacementPlayer = null;
+
+                    if (ReplacementPlayer != null)
+                        this.ReplacementPlayer = null;
                 }
                 else
                 {
-                    AFKTime++;
 
-                    bool isNotAFK = AFKTime < UltimateAFK.Instance.Config.AfkTime;
+                    var isAfk = AFKTime++ >= UltimateAFK.Instance.Config.AfkTime;
 
-                    if (!isNotAFK)
+                    if (isAfk)
                     {
-                        int num = UltimateAFK.Instance.Config.AfkTime + UltimateAFK.Instance.Config.GraceTime - this.AFKTime;
+                        var graceNumb = UltimateAFK.Instance.Config.AfkTime + UltimateAFK.Instance.Config.GraceTime - this.AFKTime;
 
-                        bool isInGrace = num > 0;
+                        var inGraceTime = graceNumb > 0;
 
-                        if (isInGrace)
+                        if (inGraceTime)
                         {
-                            var message = string.Format(UltimateAFK.Instance.Config.MsgGrace, num);
+                            var message = string.Format(UltimateAFK.Instance.Config.MsgGrace, graceNumb);
 
                             MyPlayer.Broadcast(2, message, Broadcast.BroadcastFlags.Normal, true);
                         }
@@ -166,72 +136,91 @@ namespace UltimateAFK.Handlers.Components
                         {
                             Log.Info($"{MyPlayer.Nickname} ({MyPlayer.UserId}) Detected as AFK");
 
-                            if (MyPlayer.IsAlive)
+                            //--- I am going to save the variables that I will need later
+
+                            var items = MyPlayer.Items.ToList();
+                            var role = MyPlayer.Role;
+                            var plyposition = MyPlayer.Position;
+                            var health = MyPlayer.Health;
+                            var ammo = MyPlayer.Ammo;
+                            var customitems = new List<string>();
+
+                            foreach (var item in MyPlayer.Items)
                             {
-                                var items = MyPlayer.Items;
-                                var role = MyPlayer.Role;
-                                var plyposition = MyPlayer.Position;
-                                var health = MyPlayer.Health;
-                                var ammo = MyPlayer.Ammo;
-                                ReplacementPlayer = Player.List.FirstOrDefault(p => p.Role.Type == RoleType.Spectator && !p.IsHost && !p.IsOverwatchEnabled && p != MyPlayer);
-
-                                if (ReplacementPlayer != null)
+                                if (CustomItem.TryGet(item, out var citem))
                                 {
-                                    MyPlayer.ClearInventory();
-                                    MyPlayer.SetRole(RoleType.Spectator, Exiled.API.Enums.SpawnReason.ForceClass, false);
-                                    MyPlayer.Broadcast(30, UltimateAFK.Instance.Config.MsgFspec, Broadcast.BroadcastFlags.Normal, false);
-                                    MyPlayer.SendConsoleMessage(UltimateAFK.Instance.Config.MsgFspec, "white");
-
-                                    MainHandler.ReplacingPlayers.Add(this.ReplacementPlayer, new Resources.AFKData
-                                    {
-                                        AfkComp = this,
-                                        SpawnLocation = plyposition,
-                                        Ammo = ammo,
-                                        Items = (List<ItemType>)items,
-                                        Is079 = is079,
-                                        Level = scp079Role != null ? scp079Role.Level : (byte)0,
-                                        Xp = scp079Role != null ? scp079Role.Experience : 0f,
-                                        Energy = scp079Role != null ? scp079Role.Energy : 0f,
-                                        Health = health,
-                                    });
-
-                                    if (UltimateAFK.Instance.Config.AfkCount != -1)
-                                    {
-                                        AFKCount++;
-
-                                        if (AFKCount > UltimateAFK.Instance.Config.AfkCount)
-                                        {
-                                            MyPlayer.SendConsoleMessage(UltimateAFK.Instance.Config.MsgKick, "white");
-
-                                            MyPlayer.Kick(UltimateAFK.Instance.Config.MsgKick, "[UltimateAFK]");
-                                        }
-                                    }
-
-
-                                    this.ReplacementPlayer.SetRole(role);
-
+                                    customitems.Add(citem.Name);
+                                    items.Remove(item);
                                 }
-                                else
-                                {
-                                    MyPlayer.SetRole(RoleType.Spectator);
-                                    MyPlayer.Broadcast(30, UltimateAFK.Instance.Config.MsgFspec, Broadcast.BroadcastFlags.Normal, true);
-                                    MyPlayer.SendConsoleMessage(UltimateAFK.Instance.Config.MsgFspec, "white");
-                                }
+                            }
+
+                            var list = Player.List.Where(p => p.IsDead && p.UserId != MyPlayer.UserId && !p.IsOverwatchEnabled && !p.CheckPermission("uafk.ignore") && !p.SessionVariables.ContainsKey("IsNPC"));
+                            ReplacementPlayer = list.FirstOrDefault();
+
+                            if (ReplacementPlayer == null)
+                            {
+                                Log.Debug("Unable to find replacement player moving spectator", UltimateAFK.Instance.Config.DebugMode);
+
+                                MyPlayer.SetRole(RoleType.Spectator);
+                                MyPlayer.Broadcast(30, UltimateAFK.Instance.Config.MsgFspec, Broadcast.BroadcastFlags.Normal, true);
+                                MyPlayer.SendConsoleMessage(UltimateAFK.Instance.Config.MsgFspec, "white");
                             }
                             else
                             {
-                                //It does nothing because the player is dead, so why detect him as an afk?
-                            }
+                                Log.Debug($"Replacement Player found\nNickname: {ReplacementPlayer.Nickname}\nUserID: {ReplacementPlayer.UserId}\n Role: {ReplacementPlayer.Role.Type}", UltimateAFK.Instance.Config.DebugMode);
 
+                                MainHandler.ReplacingPlayers.Add(ReplacementPlayer, new AFKData
+                                {
+                                    Position = plyposition,
+                                    Role = role,
+                                    Ammo = ammo,
+                                    Health = health,
+                                    Items = items,
+                                    CustomItems = customitems,
+                                    SCP079Role = scp079role
+
+                                });
+
+
+                                if (UltimateAFK.Instance.Config.AfkCount != -1)
+                                {
+                                    AFKCount++;
+
+                                    if (AFKCount > UltimateAFK.Instance.Config.AfkCount)
+                                    {
+                                        MyPlayer.SendConsoleMessage(UltimateAFK.Instance.Config.MsgKick, "white");
+
+                                        MyPlayer.Kick(UltimateAFK.Instance.Config.MsgKick, "[UltimateAFK]");
+                                    }
+                                }
+
+                                Log.Debug("Moving replacement player to the previous player's role", UltimateAFK.Instance.Config.DebugMode);
+
+                                this.ReplacementPlayer.SetRole(role);
+
+                                MyPlayer.SetRole(RoleType.Spectator);
+                                MyPlayer.Broadcast(30, UltimateAFK.Instance.Config.MsgFspec, Broadcast.BroadcastFlags.Normal, true);
+                                MyPlayer.SendConsoleMessage(UltimateAFK.Instance.Config.MsgFspec, "white");
+                            }
                         }
                     }
                 }
             }
-
         }
-        #endregion
 
-        #region Events
+        private IEnumerator<float> CheckAfkPerSecond()
+        {
+            while (true)
+            {
+                if (!IsDisable && Round.IsStarted)
+                {
+                    CheckAFK();
+                }
+
+                yield return Timing.WaitForSeconds(1.2f);
+            }
+        }
+
         // Technically this is not necessary since the component is destroyed when the player is destroyed but for fear of leaving a ghost component I better do this.
         public void OnDestroying(DestroyingEventArgs ev)
         {
@@ -249,7 +238,5 @@ namespace UltimateAFK.Handlers.Components
                 AFKTime = 0;
             }
         }
-
-        #endregion
     }
 }
