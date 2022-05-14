a5fb0126eb0a2e45c0e5c7c0f04d7e601f146cef
SrLicht <francoquiroz_13@live.com>
2022-05-14 18:37:34 -0300

    New version

--- UltimateAFK/UltimateAFK.cs
+++ UltimateAFK/UltimateAFK.cs
@@ -23,7 +23,7 @@ namespace UltimateAFK
 
         public override string Prefix => "Ultimate_Afk";
 
-        public override Version RequiredExiledVersion => new Version(5, 1, 1);
+        public override Version RequiredExiledVersion => new Version(5, 2, 0);
 
         public override Version Version => new Version(5, 0, 2);
 
