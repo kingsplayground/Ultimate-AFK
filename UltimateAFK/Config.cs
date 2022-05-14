f116316854d835c4108887f39dab39b0d3c48531
SrLicht <francoquiroz_13@live.com>
2022-05-14 15:03:01 -0300

    Config for CustomItems Support

--- UltimateAFK/Config.cs
+++ UltimateAFK/Config.cs
@@ -10,6 +10,9 @@ namespace UltimateAFK
         [Description("If you have any error in the plugin operation activate this and create an Issue in Github https://github.com/SrLicht/Ultimate-AFK/issues")]
         public bool DebugMode { get; set; } = false;
 
+        [Description("If you have CustomItems activated and you want the player to be given the CustomItems when replacing the player, activate this.")]
+        public bool CustomItemsSupport { get; set; } = false;
+
         [Description("If the number of players is less than this the plugin will not work.")]
         public int MinPlayers { get; set; } = 8;
 
