dee667399b77f8fac995c2bc932517bdc2792618
SrLicht <francoquiroz_13@live.com>
2022-05-16 07:47:07 -0300

    Un ejemplito para Nexus

---
+++ UltimateAFK/Handlers/Commands/KitCommand.cs
@@ -0,0 +1,32 @@
+﻿using CommandSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UltimateAFK.Handlers.Commands.SubCommands;
+
+namespace UltimateAFK.Handlers.Commands
+{
+    internal class KitCommand : ParentCommand
+    {
+        public override string Command => "kit";
+
+        public override string[] Aliases => new string[] { };
+
+        public override string Description => "Comando de ejemplo por que soy horrible explicandome";
+
+        public override void LoadGeneratedCommands()
+        {
+            if(CustomKit.Instance.Command != UltimateAFK.Instance.Config.CustomCommandPrefix)
+            {
+                RegisterCommand(CustomKit.Instance);
+            }
+        }
+
+        protected override bool ExecuteParent(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UltimateAFK/Handlers/Commands/SubCommands/CustomKit.cs
@@ -0,0 +1,26 @@
+﻿using CommandSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltimateAFK.Handlers.Commands.SubCommands
+{
+    internal class CustomKit : ICommand
+    {
+        public string Command => UltimateAFK.Instance.Config.CustomCommandPrefix;
+
+        public string[] Aliases => UltimateAFK.Instance.Config.CustomCommandAliases;
+
+        public string Description => UltimateAFK.Instance.Config.CustomCommandDescription;
+
+        public static CustomKit Instance => new CustomKit();
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            response = "Gwa gwa";
+            return true;
+        }
+    }
+}
