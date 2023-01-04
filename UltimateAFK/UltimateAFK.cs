addad3a49a0c54ea1933eba053df3432c92b2666
SrLicht <francoquiroz_13@live.com>
2023-01-03 22:35:52 -0300

    new version

--- UltimateAFK/UltimateAFK.cs
+++ UltimateAFK/UltimateAFK.cs
@@ -11,7 +11,7 @@ namespace UltimateAFK
 
         [PluginConfig] public Config Config;
         
-        [PluginEntryPoint("UltimateAFK", "1.0.0", "Checks if a player is afk for too long and if detected as afk will be replaced by a spectator.", "SrLicht")]
+        [PluginEntryPoint("UltimateAFK", "6.0.0", "Checks if a player is afk for too long and if detected as afk will be replaced by a spectator.", "SrLicht")]
         void OnEnabled()
         {
             Singleton = this;
