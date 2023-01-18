fa40dacb094156e3243e76df0ef013aa18716ae4
SrLicht <francoquiroz_13@live.com>
2023-01-18 16:13:35 -0300

    Fixing base game afk warning

--- UltimateAFK/UltimateAFK.cs
+++ UltimateAFK/UltimateAFK.cs
@@ -24,7 +24,8 @@ namespace UltimateAFK
         {
             Singleton = this;
             PluginAPI.Events.EventManager.RegisterEvents(this, new Handlers.MainHandler(Singleton));
-            if (ConfigFile.ServerConfig.GetInt("afk_time") > 0)
+            
+            if (ConfigFile.ServerConfig.GetFloat("afk_time", 90f) > 0)
             {
                 Log.Warning($"You have enabled the AFK detector of the base game, please disable it by setting &6afk_time = 0&r in &4config_gameplay.txt&r");
             }
