32f8c93b2fa98ed722afafd701b2e7a5929519d3
SrLicht <francoquiroz_13@live.com>
2023-06-24 19:31:11 -0300

    Rebuild for 13.1
    Maybe this fix the error on GetReplacement

--- UltimateAFK/UltimateAFK.cs
+++ UltimateAFK/UltimateAFK.cs
@@ -14,7 +14,7 @@ namespace UltimateAFK
         [PluginConfig] public Config Config;
 
         [PluginPriority(LoadPriority.High)]
-        [PluginEntryPoint("UltimateAFK", "6.2.4", "Checks if a player is afk for too long and if detected as afk will be replaced by a spectator.", "SrLicht")]
+        [PluginEntryPoint("UltimateAFK", "6.3.0", "Checks if a player is afk for too long and if detected as afk will be replaced by a spectator.", "SrLicht")]
         void OnEnabled()
         {
             Singleton = this;
